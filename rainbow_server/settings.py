5c13eee9e799329cae3bdc0b27dde6cf466e76da
一维 <persenlee90@gmail.com>
2019-03-23 10:47:30 +0800

    modify dbname

--- rainbow_server/settings.py
+++ rainbow_server/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rainbow',
+        'NAME': 'bearhub',
         'USER': 'root',
         'PASSWORD': '123456',
         'HOST': 'localhost',
